b934e7ab089167da9e0590dd8c47f9181bb3d113
Jason Hansen <jason@slack.io>
2025-07-23 21:17:48 -0700

    theme: switch to tokyonight

--- init.lua
+++ init.lua
@@ -198,15 +198,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    'Tsuzat/NeoSolarized.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd [[ colorscheme NeoSolarized ]]
-    end,
-  },
-
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -817,7 +808,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'NeoSolarized'
+      vim.cmd.colorscheme 'tokyonight'
     end,
   },
 
